c397c8684b42f5bf12ecdd8697f9f148c8021c74
Mickae Truman <mtruman@nyit.edu>
2019-04-28 18:19:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     sql = <<-SQL 
     SELECT *
     FROM students 
-    WHERE students.grade = ?
+    WHERE students.grade = 10
     SQL
     DB[:conn].execute(sql, x).collect do |row|
       self.new_from_db(row)
