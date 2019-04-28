864a2c79d239fa2eed92018919aef9dabd51f50a
Mickae Truman <mtruman@nyit.edu>
2019-04-28 18:11:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,8 @@ class Student
     sql = <<-SQL 
     SELECT *
     FROM students 
-    WHERE students.grade =10
+    WHERE students.grade = 10
+    LIMIT 1
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
